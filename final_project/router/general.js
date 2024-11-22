e9a419935d36e66e9bebb81f82f6cdcaa678de7d
vamegah <makvim@yahoo.com>
2024-11-21 21:53:45 -0500

    Added registered user can login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,14 +26,14 @@ public_users.post("/register", (req, res) => {
         // Check if the user does not already exist
         if (!doesExist(username)) {
             // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
         } else {
-            return res.status(404).json({message: "User already exists!"});
+            return res.status(404).json({ message: "User already exists!" });
         }
     }
     // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
+    return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
