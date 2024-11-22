fc767c6146bbeb375ca7543dc30fe4d817dd8386
vamegah <makvim@yahoo.com>
2024-11-21 21:27:47 -0500

    added Get book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,15 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.status(200).send(JSON.stringify(books, null, 4));
+  return res.status(404).json({message: "An error has occured trying to retrieve all the books"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.status(200).send(JSON.stringify(books[req.params.isbn], null, 4))
+  return res.status(300).json({message:`Book with ISBN ${isbn} not found.` });
  });
   
 // Get book details based on author
