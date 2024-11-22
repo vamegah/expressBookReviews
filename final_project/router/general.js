e78b88cc7b65691cccacd76b98b48cb46d39e8c8
vamegah <makvim@yahoo.com>
2024-11-21 21:44:55 -0500

    added Get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,7 +73,12 @@ public_users.get('/title/:title', function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        return res.status(200).send(b[isbn]);
+    } else {
+        return res.status(404).json({ message: `Book with ISBN ${isbn} not found.` });
+    }
 });
 
 module.exports.general = public_users;
